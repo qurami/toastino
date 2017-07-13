be99037474dfe334b88868bbab2e0af4cd8c3b47
Marco Bramini <braminim@gmail.com>
2017-07-13 19:55:04 +0200

    Changed Map<int, Future<ComponentRef>> with List<ComponentRef>. Added toast methods to handle removal. Added container method to update id after removal. Added some basic css.

--- lib/src/toast/toast_component.dart
+++ lib/src/toast/toast_component.dart
@@ -6,8 +6,10 @@ import 'package:angular2/core.dart';
 @Component(
   selector: 'toast',
   templateUrl: 'toast_component.html',
+  styleUrls: const ['toast_component.css'],
 )
 class Toast{
+  int id;
   String title, message;
 
   Toast(){
@@ -17,6 +19,12 @@ class Toast{
 
   void setTitle(String title){
     this.title = title;
+  }
+
+  int getId() => this.id;
+
+  void setId(int id) {
+    this.id = id;
   }
 
 }
--- lib/src/toast_container_component.dart
+++ lib/src/toast_container_component.dart
@@ -8,38 +8,49 @@ import 'package:angular2/angular2.dart';
 import 'package:angular_components/angular_components.dart';
 
 @Component(
-  selector: 'toast-container',
-  template: '<span #toastContainer></span>'
-
+    selector: 'toast-container',
+    template: '<div class="toastContainer" #toastContainer></div><button (click)="createToast()">create</button><button (click)="removeFirstToast()">remove first</button>',
+    styleUrls: const ['toast_container_component.css'],
 )
-class ToastContainer implements OnInit{
+class ToastContainer implements OnInit {
   DynamicComponentLoader toastLoader;
-  @ViewChild('toastContainer', read:ViewContainerRef)
+  @ViewChild('toastContainer', read: ViewContainerRef)
   ViewContainerRef containerRef;
 
-  Map<int, Future<ComponentRef>> toastLoaded;
+  List<ComponentRef> toastLoaded;
   int next;
 
-  ToastContainer(this.toastLoader, this.containerRef){
-    this.toastLoaded = new Map<int, Future<ComponentRef>>();
+  ToastContainer(this.toastLoader, this.containerRef) {
+    this.toastLoaded = new List<ComponentRef>();
     this.next = 0;
   }
 
   @override
   ngOnInit() async {
-    createToast();
-    createToast();
-    removeToast(0);
   }
 
-  void createToast(){
-    toastLoaded[this.next] = toastLoader.loadNextToLocation(Toast, containerRef);
-    toastLoaded[this.next].then((ComponentRef c){Toast toast = c.instance; toast.setTitle("ciao");});
-    next++;
-    print(next);
+  // Implementing FIFO style management
+  // createToast creates a new toast component, puts data in it and save it into map using sequential keys
+  void createToast() {
+    toastLoader.loadNextToLocation(Toast, containerRef).then((ComponentRef cRef) {
+      this.toastLoaded.add(cRef);
+      Toast toast = cRef.instance;
+      toast.setId(this.next);
+      toast.setTitle("ciao"+this.next.toString());
+      next++;
+      print(next);
+    });
   }
 
-  void removeToast(int id){
-    toastLoaded[id].then((ComponentRef c){print(c.toString()); c.destroy();});
+  //
+  void removeFirstToast() {
+    this.toastLoaded.first.destroy();
+    this.toastLoaded.removeAt(0);
+    updateActiveToasts();
+    next--;
+  }
+  
+  void updateActiveToasts(){
+    this.toastLoaded.forEach((ComponentRef cRef) => cRef.instance.setId(cRef.instance.getId()-1));
   }
 }
