2aee5c8e405a90a0cc699fe379699709dd0a91dd
Marco Bramini <braminim@gmail.com>
2017-07-14 18:00:10 +0200

    Added Timer for CSS timeline. Added external component template (for testing). Edited successToast and createToast method. Edited methods to match new Toast methods. New logic when updating Toasts.

--- lib/src/toast_container_component.dart
+++ lib/src/toast_container_component.dart
@@ -2,11 +2,12 @@ import 'package:angular2/core.dart';
 import 'package:toastino/src/toast/toast_component.dart';
 import 'package:toastino/src/toast/success_toast_component.dart';
 import 'dart:core';
+import 'dart:async';
 import 'package:angular2/angular2.dart';
 
 @Component(
     selector: 'toast-container',
-    template: '<span #toastContainer></span><button (click)="successToast()" style="position: absolute; bottom: 100px;">create</button><button (click)="removeFirstToast()" style="position: absolute; bottom: 80px;">remove first</button>',
+    templateUrl: 'toast_container_component.html',
     styleUrls: const ['toast_container_component.css'],
 )
 class ToastContainer implements OnInit {
@@ -29,22 +30,23 @@ class ToastContainer implements OnInit {
   ngOnInit() async {
   }
 
-  void successToast(){
-    createToast(SuccessToast);
+  void successToast(String title, String message){
+    createToast(SuccessToast, title, message);
   }
 
   // Implementing FIFO style management
   // createToast creates a new toast component, puts data in it and save it into map using sequential keys
-  void createToast(Type toastType) {
+  void createToast(Type toastType, String title, String message) {
     toastLoader.loadNextToLocation(toastType, containerRef).then((ComponentRef cRef) {
       this.activeToasts.add(cRef);
       Toast toast = cRef.instance;
-      toast.setId(this.next);
-      toast.setTitle("ciao"+this.next.toString());
-      toast.action();
+      toast.setPosition(this.next);
+      toast.setTitle(title);
+      toast.setMessage(message);
+      toast.updateCSS();
       next++;
-      print(next);
-      //Timer to remove toast
+      new Timer(new Duration(milliseconds: 400), () => toast.action());
+      //new Timer(new Duration(seconds: 5), () => removeFirstToast());
     });
   }
 
@@ -55,8 +57,12 @@ class ToastContainer implements OnInit {
     updateActiveToasts();
     next--;
   }
-  // updateActiveToast updates id after toast removal
+
+  // updateActiveToast updates ids after toast removal
   void updateActiveToasts(){
-    this.activeToasts.forEach((ComponentRef cRef) => cRef.instance.setId(cRef.instance.getId()-1));
+    for (ComponentRef cRef in this.activeToasts){
+      cRef.instance.setPosition(cRef.instance.getPosition()-1);
+      cRef.instance.updateCSS();
+    }
   }
 }
