7869c75453c63315ec0bedd762de9c513ec9dfff
Marco Bramini <braminim@gmail.com>
2017-07-20 18:03:24 +0200

    New initialization.

--- example/lib/toastino_example_component.dart
+++ example/lib/toastino_example_component.dart
@@ -5,18 +5,12 @@ import 'package:toastino/toastino.dart';
 @Component(
   selector: 'toastino-example',
   templateUrl: 'toastino_example_component.html',
-  directives: const [ToastManagerComponent],
 )
 class ToastinoExampleComponent{
-  DynamicComponentLoader toastComponentLoader;
-
-  @ViewChild('toastino', read: ViewContainerRef)
-  ViewContainerRef viewContainerRef;
-
   ToastManagerComponent _toastManager;
 
-  ToastinoExampleComponent(this.toastComponentLoader, this.viewContainerRef){
-    this._toastManager = new ToastManagerComponent(toastComponentLoader, viewContainerRef);
+  ToastinoExampleComponent(DynamicComponentLoader toastComponentLoader, ViewContainerRef viewContainerRef){
+    _toastManager = new ToastManagerComponent(toastComponentLoader, viewContainerRef);
   }
 
   void toast(String title){
