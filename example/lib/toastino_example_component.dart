c32df53592eb6fe8f5e7c4a6b095daff9ae50576
Marco Bramini <braminim@gmail.com>
2017-07-21 14:19:01 +0200

    Added copyright lines.

--- example/lib/toastino_example_component.dart
+++ example/lib/toastino_example_component.dart
@@ -1,3 +1,6 @@
+// Copyright (c) 2017, Marco Bramini. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'dart:core';
 import 'package:angular2/core.dart';
 import 'package:toastino/toastino.dart';
