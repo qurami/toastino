c182887c5d6d9ed97d0befb0fa5d9abba5dbec50
Marco Bramini <braminim@gmail.com>
2017-07-21 14:11:21 +0200

    Added draft comments to toast_component.

--- lib/src/toast_component.dart
+++ lib/src/toast_component.dart
@@ -1,13 +1,21 @@
+// Copyright (c) 2017, Marco Bramini. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'dart:core';
 import 'package:angular2/core.dart';
 
+/// A Toast component.
+/// It is created by a [ToastManager], which handles its positioning and removal too.
+///
+/// [ToastComponent]'s [_position] affects the host's top property. TODO: optional positioning of toasts and mobile
+///
 @Component(
   selector: 'toast',
   template: '{{title}}',
-  styleUrls: const['toast_component.css'],
-  host: const {'[style.top.px]' : 'position * 60'},
+  styleUrls: const ['toast_component.css'],
+  host: const {'[style.top.px]': 'position * 60'},
 )
-class ToastComponent{
+class ToastComponent {
   String _title;
   String get title => _title;
 
@@ -16,12 +24,14 @@ class ToastComponent{
 
   ToastComponent();
 
+  /// Initialize [ToastComponent].
   void init(String title, int position) {
     _title = title;
     _position = position;
   }
 
-  void decreasePosition(){
+  /// Decrease [ToastComponent]'s position by one.
+  void decreasePosition() {
     _position--;
   }
 }
